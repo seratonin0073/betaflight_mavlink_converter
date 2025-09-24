25b00b31ab49c86460a00fd20f240c737c0b0aa6
seratonin0073 <mizko2000y@gmail.com>
2025-09-24 21:50:32 +0300

    Add biuld_pi.sh

---
+++ build_pi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "🔨 Збірка для Raspberry Pi..."
+
+mkdir -p build
+cd build
+
+cmake -DARM=ON ..
+
+make -j$(nproc)
+
+if [ -f "bfmavconverter" ]; then
+    echo "✅ Програму успішно зібрано для Raspberry Pi!"
+    chmod +x
+fi
